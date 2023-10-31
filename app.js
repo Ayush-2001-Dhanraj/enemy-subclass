57a2eb5887dc8f49dfa2f9e1d4b46d10c065ccef
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2023-10-31 14:48:44 +0530

    Add boiler plate

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 /** @type {HTMLCanvasElement} */
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
-const CANVAS_WIDTH = (canvas.width = 500);
-const CNVAS_HEIGHT = (canvas.height = 700);
+
+document.addEventListener("load", () => {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+  const CANVAS_WIDTH = (canvas.width = 500);
+  const CANVAS_HEIGHT = (canvas.height = 700);
+
+  class Game {
+    constructor() {
+      this.enemies = [];
+    }
+    update() {}
+    draw() {}
+    #addnewEnemy() {}
+  }
+
+  class Enemy {
+    constructor() {}
+    update() {}
+    draw() {}
+  }
+
+  function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+});
